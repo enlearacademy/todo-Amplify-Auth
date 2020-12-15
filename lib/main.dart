ecf252f769814613dcd2cd6973ea44beff641861
PandukaNandara <pandukanandara@outlook.com>
2020-12-15 23:33:22 +0530

    Updated app level gradle.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+
+import 'package:amplify_core/amplify_core.dart';
 import 'package:flutter/material.dart';
+import 'package:todo_amplify/amplifyconfiguration.dart';
 
 void main() {
   runApp(
@@ -21,8 +25,33 @@ class AmplifyTODO extends StatefulWidget {
 }
 
 class _AmplifyTODOState extends State<AmplifyTODO> {
+  bool _amplifyConfigured = false;
+  Amplify _amplifyInstance = Amplify();
+
+  Future<void> _configureAmplify() async {
+    try {
+      AmplifyAuthCognito auth = AmplifyAuthCognito();
+      _amplifyInstance.addPlugin(
+        authPlugins: [auth],
+      );
+      await _amplifyInstance.configure(amplifyconfig);
+
+      setState(() => _amplifyConfigured = true);
+    } catch (e) {
+      print(e);
+      setState(() => _amplifyConfigured = false);
+    }
+    setState(() => _amplifyConfigured = true);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _configureAmplify();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold();
   }
 }
