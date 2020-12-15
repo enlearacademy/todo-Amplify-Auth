35ce1cecb1208e39c24cd5b553d9932b17ea5be9
PandukaNandara <pandukanandara@outlook.com>
2020-12-16 01:23:50 +0530

    Updated emai_confirmation_screen.dart

--- lib/screens/email_confirmation_screen.dart
+++ lib/screens/email_confirmation_screen.dart
@@ -1,4 +1,3 @@
-import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:todo_amplify/screens/main_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -15,6 +14,8 @@ class EmailConfirmationScreen extends StatelessWidget {
   final TextEditingController _confirmationCodeController =
       TextEditingController();
 
+  final _formKey = GlobalKey<FormFieldState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,29 +25,37 @@ class EmailConfirmationScreen extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            Text(
-              "An email confirmation code is sent to $email. Please type the code to confirm your email.",
-              style: Theme.of(context).textTheme.headline6,
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              controller: _confirmationCodeController,
-              decoration: InputDecoration(labelText: "Confirmation Code"),
-            ),
-            RaisedButton(
-              onPressed: () => _submitCode(context),
-              child: Text("CONFIRM"),
-            ),
-          ],
+        child: Form(
+          child: Column(
+            children: [
+              Text(
+                "An email confirmation code is sent to $email. Please type the code to confirm your email.",
+                style: Theme.of(context).textTheme.headline6,
+              ),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                controller: _confirmationCodeController,
+                decoration: InputDecoration(labelText: "Confirmation Code"),
+                validator: (value) => value.length != 6
+                    ? "The confirmation code is invalid"
+                    : null,
+              ),
+              RaisedButton(
+                onPressed: () => _submitCode(context),
+                child: Text("CONFIRM"),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 
   Future<void> _submitCode(BuildContext context) async {
-    // TODO: Submit the code to Amplify
+    if(_formKey.currentState.validate()){
+      final confirmationCode = _confirmationCodeController.text;
+      // TODO: Submit the code to Amplify
+    }
   }
 
   void _gotoMainScreen(BuildContext context) {
